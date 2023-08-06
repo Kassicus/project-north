38b4bcbe812ec363385cabd6a6ad92fcbabc0126
Kason <kason.suchow@gmail.com>
2023-08-06 11:58:20 -0600

    Breakdown trees > felled trees > raw logs

--- tree.py
+++ tree.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 
 import gl
 
@@ -23,7 +24,7 @@ class OakTree(pygame.sprite.Sprite):
     def destroy(self):
         child = FelledOakTree(self.pos.x, self.pos.y + self.rect.height / 2, self.world_ref)
         self.world_ref.camera.add(child)
-        self.world_ref.tree_container.add(child)
+        self.world_ref.felled_tree_container.add(child)
         self.kill()
 
 class FelledOakTree(pygame.sprite.Sprite):
@@ -33,8 +34,34 @@ class FelledOakTree(pygame.sprite.Sprite):
         self.pos = pygame.math.Vector2(x, y)
         self.world_ref = world
 
+        self.health = 1
+
         self.image = pygame.Surface([240, 30])
         self.image.fill(gl.color.cyan)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
+
+    def update(self):
+        if self.health <= 0:
+            self.destroy(randint(2, 5))
+
+    def destroy(self, children: int):
+        for x in range(children):
+            child = RawOakLog(randint(self.pos.x - 15, self.pos.x + self.rect.width + 15), randint(self.pos.y - 15, self.pos.y + self.rect.height + 15), self.world_ref)
+            self.world_ref.camera.add(child)
+            self.world_ref.log_container.add(child)
+            
+        self.kill()
+
+class RawOakLog(pygame.sprite.Sprite):
+    def __init__(self, x: int, y: int, world: object):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.pos = pygame.math.Vector2(x, y)
+        self.world_ref = world
+
+        self.image = pygame.Surface([30, 60])
+        self.image.fill(gl.color.red)
         self.rect = self.image.get_rect()
         self.rect.center = self.pos
 
--- world.py
+++ world.py
@@ -15,6 +15,8 @@ class World():
 
         self.collidables = pygame.sprite.Group()
         self.tree_container = pygame.sprite.Group()
+        self.felled_tree_container = pygame.sprite.Group()
+        self.log_container = pygame.sprite.Group()
 
         self.camera.add(self.player)
 
@@ -29,17 +31,32 @@ class World():
             self.tree_container.add(t)
             self.collidables.add(t)
 
+    def check_trees_hit(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos() + gl.global_offset
+        for tree in self.tree_container:
+            if tree.rect.left < mouse_x < tree.rect.right:
+                if tree.rect.top < mouse_y < tree.rect.bottom:
+                    tree.health -= 1
+
+    def check_felled_trees_hit(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos() + gl.global_offset
+        for felled_tree in self.felled_tree_container:
+            if felled_tree.rect.left < mouse_x < felled_tree.rect.right:
+                if felled_tree.rect.top < mouse_y < felled_tree.rect.bottom:
+                    felled_tree.health -= 1
+
     def draw(self):
         self.camera.camera_draw(self.player)
 
     def update(self):
         self.camera.update()
         self.tree_container.update()
+        self.felled_tree_container.update()
+        self.log_container.update()
 
         for event in gl.events:
             if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_x, mouse_y = pygame.mouse.get_pos() + gl.global_offset
-                for tree in self.tree_container:
-                    if tree.pos.x < mouse_x < tree.pos.x + tree.rect.width:
-                        if tree.pos.y < mouse_y < tree.pos.y + tree.rect.height:
-                            tree.health -= 1
+                if event.button == 1:
+                    print("click")
+                    self.check_trees_hit()
+                    self.check_felled_trees_hit()
